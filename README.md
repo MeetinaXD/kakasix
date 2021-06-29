9790ee89879e7be259a499c7ce98371a7b63a3f2
MeetinaXD <capitaycity@gmail.com>
2021-06-29 15:24:21 +0800

    README add

---
+++ README.md
@@ -0,0 +1,32 @@
+## Kakasix
+A simple and quick tool to translate Japanese to hiragana and jisroman.
+
+This tool relies on **kakasi** to run properly.
+[Visit this project](https://github.com/loretoparisi/kakasi) to see how to install **kakasi**.
+
+### Usage
+command line
+```shell
+# input
+kakasix 桜
+
+#output
+桜: さくら sakura
+```
+or
+```shell
+#input
+kakasix
+
+#interact
+桜
+桜: さくら sakura
+心
+心: こころ kokoro
+```
+**tips:** You can press Enter twice to quit.
+
+### Thanks to
+
+**Hironobu Takahashi.**
+[KAKASI - Kanji Kana Simple Inverter](http://kakasi.namazu.org/index.html.en)
---
+++ main.js
@@ -0,0 +1,50 @@
+const shell = require("shelljs")
+const colors = require("colors");
+const argv = require('yargs').argv;
+const stdio = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function readLine() {
+  return new Promise(resolve => {
+    stdio.on('line', resolve)
+  })
+}
+
+function close() {
+  stdio.close()
+}
+
++async function(){
+  argv._.forEach(s => {
+    convert(s)
+    close()
+  })
+
+  if (argv._.length){
+    return ;
+  }
+  let counter = 0;
+  while(true){
+    const t = await readLine()
+    if (t.trim() == ''){
+      counter ++;
+    } else {
+      convert(t)
+    }
+    if (counter == 2){
+      close()
+    }
+  }
+}()
+
+function convert(sentence){
+  const jh = shell.exec(`echo "${sentence}" | kakasi -iutf8 -outf8 -JH`, {
+    silent: true
+  }).stdout.trim()
+  const ja = shell.exec(`echo "${sentence}" | kakasi -iutf8 -outf8 -Ja`, {
+    silent: true
+  }).stdout.trim()
+  console.log(`${sentence}: ${colors.green(jh)} ${colors.green(ja)}\n`);
+}
